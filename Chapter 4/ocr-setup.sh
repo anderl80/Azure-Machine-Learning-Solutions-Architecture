3b1ff661892829c36a1164b636e7708160916a78
Andreas Hopfgartner <23196828+anderl80@users.noreply.github.com>
2022-04-21 20:15:00 +0000

    Separate into IaC shell scripts and Jupyter code

---
+++ Chapter 4/ocr-setup.sh
@@ -0,0 +1,14 @@
+export SERVICENAME=computervision
+export REGION=westeurope
+export RESOURCEGROUP=rg-aml-book-cogsrv
+
+az group create --name $RESOURCEGROUP --location $REGION
+az cognitiveservices account create --name $SERVICENAME --resource-group $RESOURCEGROUP --kind ComputerVision --sku F0 --location $REGION --yes
+
+export SUBSCRIPTION_ID=$(az account show --query id -o tsv)
+export ACCOUNT_REGION=$(az cognitiveservices account show --resource-group $RESOURCEGROUP --name $SERVICENAME --query location --output tsv)
+export ACCOUNT_KEY=$(az cognitiveservices account keys list --resource-group $RESOURCEGROUP --name $SERVICENAME --query key1 --output tsv)
+
+echo "ACCOUNT_REGION=$ACCOUNT_REGION" > .env
+echo "ACCOUNT_KEY=$ACCOUNT_KEY" >> .env
+echo "SUBSCRIPTION_ID=$SUBSCRIPTION_ID" >> .env
