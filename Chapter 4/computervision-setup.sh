a64815fa40a997e88be0baeaaa885a95cd579164
Andreas Hopfgartner <23196828+anderl80@users.noreply.github.com>
2022-05-04 19:38:31 +0000

    Update setup file

--- Chapter 4/computervision-setup.sh
+++ Chapter 4/computervision-setup.sh
@@ -3,7 +3,7 @@ export REGION=westeurope
 export RESOURCEGROUP=rg-aml-book-cogsrv
 
 az group create --name $RESOURCEGROUP --location $REGION
-#az cognitiveservices account create --name $SERVICENAME --resource-group $RESOURCEGROUP --kind ComputerVision --sku F0 --location $REGION --yes
+az cognitiveservices account create --name $SERVICENAME --resource-group $RESOURCEGROUP --kind ComputerVision --sku F0 --location $REGION --yes
 
 export SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 export ACCOUNT_REGION=$(az cognitiveservices account show --resource-group $RESOURCEGROUP --name $SERVICENAME --query location --output tsv)
